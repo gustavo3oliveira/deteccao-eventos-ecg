72ff85c78febb7d999c598b8d5358e364a27b74f
betoceccato <ceccato.beto@gmail.com>
2021-06-27 17:57:12 -0300

    Criação do diretório para o desenvolvimento do detector

---
+++ detector_PT/main_PT.m
@@ -0,0 +1,5 @@
+% Author: Gustavo Henrique de Oliveira, Maria Antònia Marzo Barceló, Roberto Costa Ceccato
+% Date: 27/06/2021
+% Description: Script principal para o projeto da Parte B (Prof. Sérgio)
+
+%% Tratamento de dados 
